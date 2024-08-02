0cb2002d9da68349a606ba478635c765cfb86338
Soumajit Das <dbubai61@gmail.com>
2024-08-02 13:33:03 +0530

    Add user & video model

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import {Schema,model} from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,  //cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String,  //cloudinary url
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    viewsCount: {
+        type: Number,
+        default: 0
+    },
+    likesCount: {
+        type: Number,
+        default: 0
+    },
+    commentsCount: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+},{timestamps:true});
+
+export const Video = model('Video',videoSchema);
