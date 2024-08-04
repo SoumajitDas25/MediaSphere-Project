32ba724645ade1eb319217e7ca0cd31399be224f
Soumajit Das <dbubai61@gmail.com>
2024-08-05 00:32:32 +0530

    Add logout & other functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import {User} from '../models/user.model.js'
 import {fileUpload} from '../utils/cloudinary.js'
+import jwt from "jsonwebtoken";
 
 const cookieOptions={
     httpOnly:true,
@@ -145,7 +146,133 @@ const loginUser = asyncHandler(async (req,res)=>{
 
 })
 
+const logoutUser = asyncHandler(async (req,res)=>{
+
+    //find & update user document by setting refresh token to undefined
+    const updatedUser = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:{
+                refreshToken: 1 // this will remove this field from the doc
+            }
+        },
+        {
+            new:true
+        }
+    );
+
+    if(!updatedUser)
+    {
+        throw new ApiError(500,"Something went wrong while Logout");
+    }
+
+    //clear cookies while sending response
+    return res
+    .status(200)
+    .clearCookie("accessToken",cookieOptions)
+    .clearCookie("refreshToken",cookieOptions)
+    .json(
+        new ApiResponse(200,true,"Logout Successful")
+    );
+})
+
+const refreshAccessToken = asyncHandler(async (req,res)=>{
+    //fetch refresh token from cookies or req body(in case of mob app) or header(in case of mob app) 
+    let incomingRefreshToken;
+    if((req.cookies && req.cookies.refreshToken) || 
+    (req.body && req.body.refreshToken) ||
+    (req.header && req.header.Authorization))
+    {
+        incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken || req.header("Authorization").replace("Bearer ","");
+    }
+    if(!incomingRefreshToken)
+    {
+        throw new ApiError(401,"Unauthorized Request");
+    }
+
+    //decode the token
+    const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET);
+    if(!decodedToken)
+    {
+        throw new ApiError(500,"Something went wrong while decoding Access Token");
+    }
+    
+    //find the user document by id contained in the token
+    const user =await User.findById(decodedToken._id);
+    if(!user)
+    {
+        throw new ApiError(401,"Invalid Refresh Token");
+    }
+    
+    //check the equality of the incoming token with the token contained in the user doc
+    if(incomingRefreshToken !== user.refreshToken)
+    {
+        throw new ApiError(401,"Refresh Token is expired or used");
+    }
+
+    //generate new access & refresh tokens
+    const {refreshToken:newRefreshToken,accessToken:newAccessToken} = await generateAccessAndRefreshToken(user._id);
+
+    if(!newAccessToken || !newRefreshToken)
+    {
+        throw new ApiError(500,"Something went wrong while generating tokens");
+    }
+
+    //send new access & refresh tokens as cookies along with the response
+    return res.status(200)
+    .cookie("accessToken",newAccessToken,cookieOptions)
+    .cookie("refreshToken",newRefreshToken,cookieOptions)
+    .json(new ApiResponse(
+        200,
+        {
+            refreshToken:newRefreshToken,
+            accessToken:newAccessToken
+        },
+        "Access Token Refreshed"
+    ));
+    
+})
+
+const changeCurrentPassword = asyncHandler(async (req,res)=>{
+
+    //fetch old & new password from req body
+    const {oldPassword,newPassword} = req.body;
+    if(!oldPassword || !newPassword)
+    {
+        throw new ApiError(400,"Password fields are required");
+    }
+
+    //get user document by id from req.user
+    const user = await User.findById(req.user?._id);
+    
+    //validate the old password with the password contained in the user doc
+    const isPasswordValidated = await user.isPasswordCorrect(oldPassword);
+    if(!isPasswordValidated)
+    {
+        throw new ApiError(400,"Invalid Current Password");
+    }
+
+    //add the new password to the password field of  user doc
+    user.password = newPassword;
+
+    //save the user doc
+    const updatedUser = await user.save({validateBeforeSave:false});
+    if(!updatedUser)
+    {
+        throw new ApiError(500,"Something went wrong while updating User document");
+    }
+
+    res.status(200)
+    .json(
+        new ApiResponse(200,{},"Password Changed Successfully")
+    );
+
+})
+
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword
 }
