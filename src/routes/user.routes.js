68a84fa1f33cbc55d7235dd1af9d23eea718a7c6
Soumajit Das <dbubai61@gmail.com>
2024-08-05 00:40:39 +0530

    Add user router

---
+++ src/routes/user.routes.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { 
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: 'avatar',
+            maxCount: 1
+        },
+        {
+            name: 'coverImage',
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+router.route("/login").post(loginUser);
+//secured routes
+router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").patch(verifyJWT,changeCurrentPassword);
+
+export default router;
