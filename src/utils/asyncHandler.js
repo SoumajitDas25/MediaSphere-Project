b6c4524434ee3b50535b9dd913460fc5c0ae0c4f
Soumajit Das <dbubai61@gmail.com>
2024-09-13 21:31:35 +0530

    Update asyncHanlder method for better error responding

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,5 @@
+import { SERVER_ERROR_MESSAGE } from "../constants.js";
+
 const asyncHandler = (requestHandler) => async (req, res, next) => {
     try 
     {
@@ -5,7 +7,52 @@ const asyncHandler = (requestHandler) => async (req, res, next) => {
     } 
     catch(error) 
     {
-        next(error)
+        //some unexpected server error
+        if(!error.statusCode)
+        { 
+            //send a server error response to the client
+            res.status(500).json(
+                {
+                    error: SERVER_ERROR_MESSAGE,
+                    success: false
+                }
+            );
+        }
+
+        // next(error)
+        //check whether it is client error or server error
+        if(error.statusCode < 500)
+        {
+            //client error
+
+            //send the client error response to the client
+            res.status(error.statusCode).json(
+                {
+                    error: error.message,
+                    statusCode: error.statusCode,
+                    success: false
+                }
+            );
+        }
+        else
+        {
+            //server error
+
+            console.log("Server Error: ",error.message); //log the server error message
+
+             // Extracting the file path & line no.
+            const stackLines = error.stack.split('\n');
+            const relevantLine = stackLines[1].trim();
+            console.log('Error occurred in: ', relevantLine);
+
+            //send a server error response to the client
+            res.status(error.statusCode).json(
+                {
+                    error: SERVER_ERROR_MESSAGE,
+                    success: false
+                }
+            );
+        }
     }
 }
 
